ce2a011523afca4c536791427de4c9025e8a4f4d
andy-agv <andyageenkov@gmx.com>
2023-07-14 15:04:45 +0400

    add homepage loading toast

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,42 @@
 import './HomePage.css';
 import Table from '../../components/Table.jsx'
 import HeaderBox from '../../components/HeaderBox.jsx'
+import { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 export default function HomePage({commodities}) {
-  if (commodities === null) {
-    return <div>Loading...</div>;  // Show a loading message while the data is being fetched
-}  
+  const [isLoading, setIsLoading] = useState(true);
+  const toastIdRef = useRef(null);
+
+  useEffect(() => {
+    if (isLoading) {
+      toastIdRef.current = toast.loading('Getting latest data', {
+        iconTheme: {
+          primary: 'var(--accent)',
+          secondary: 'white',
+        },
+      })
+    } else {
+      toast.dismiss(toastIdRef.current);
+    }
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (commodities !== null) {
+      setIsLoading(false);
+    }
+  }, [commodities]);
   
+  if (isLoading) {
+    return(
+      <main>
+        <div className="d-flex flex-column align-items-center">
+            <HeaderBox text={'All commodities'} add={false} fav={false}/>
+        </div>
+      </main>
+    );  // Show a loading message while the data is being fetched
+  }
+
   return (
       <main>
         <div className="d-flex flex-column align-items-center">
